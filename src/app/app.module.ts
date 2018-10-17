da1575f5bbbb4e03c4016dbd1158df3a90b59969
Erica Dantas do Nascimento <erica.dantas@academico.ifrn.edu.br>
2018-10-17 14:03:57 -0300

    vdc

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,8 @@ import { TabsPage } from '../pages/tabs/tabs';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { MensagemPage } from '../pages/mensagem/mensagem';
+import { RespostaPage } from '../pages/resposta/resposta';
 
 @NgModule({
   declarations: [
@@ -17,7 +19,9 @@ import { SplashScreen } from '@ionic-native/splash-screen';
     AboutPage,
     ContactPage,
     HomePage,
-    TabsPage
+    TabsPage,
+    MensagemPage,
+    RespostaPage
   ],
   imports: [
     BrowserModule,
@@ -29,7 +33,9 @@ import { SplashScreen } from '@ionic-native/splash-screen';
     AboutPage,
     ContactPage,
     HomePage,
-    TabsPage
+    TabsPage,
+    MensagemPage,
+    RespostaPage
   ],
   providers: [
     StatusBar,
